2bc622c2b4b7de34c3ccd31b3f531c7fad0dc947
hanifamar17 <hanifamar17@gmail.com>
2024-07-11 20:10:16 +0700

    feat: add modal input khodam

--- app.py
+++ app.py
@@ -1,38 +1,57 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import random
+import json
 
 app = Flask(__name__)
 app.secret_key = b"=(7\x06't\xf7.*Z\x9c9\x19B\x15\xae\x13\xae\xd1\x10:\xff\x90\x02"
 
-khodams=[
-    "siluman ayam geprek", "kodok albino", "trenggiling bikini bottom", "kulit ayam tiren", "gajah india",    
-    "tikus terbang", "walang kayang"
-]
 
-@app.route('/')
+def load_khodams():
+    with open("khodams.json", "r") as f:
+        return json.load(f)
+
+
+# Fungsi untuk menulis khodams ke file JSON
+def save_khodams(khodams):
+    with open("khodams.json", "w") as f:
+        json.dump(khodams, f, indent=4)
+
+
+@app.route("/")
 def home():
-    return redirect(url_for('cek_khodam'))
+    return redirect(url_for("cek_khodam"))
 
-@app.route('/cek-khodam', methods=['GET', 'POST'])
+
+@app.route("/cek-khodam", methods=["GET", "POST"])
 def cek_khodam():
-    random_word=None
-    if request.method == 'POST':
-        query = request.form.get('query')
+    khodams = load_khodams()
+    if request.method == "POST":
+        query = request.form.get("query")
         if query:
             random_word = random.choice(khodams)
 
-            session['query'] = query
-            session['random_word'] = random_word
-        return redirect(url_for('cek_khodam_hasil'))
-    
-    return render_template('cek_khodam.html')
+            session["query"] = query
+            session["random_word"] = random_word
+        return redirect(url_for("cek_khodam_hasil"))
+    return render_template("cek_khodam.html")
 
-@app.route('/cek-khodam/hasil', methods=['GET', 'POST'])
+
+@app.route("/cek-khodam/hasil", methods=["GET", "POST"])
 def cek_khodam_hasil():
-    query = session.get('query')
-    random_word = session.get('random_word', '-')
-    return render_template('hasil.html', random_word=random_word, query=query)
-    
+    query = session.get("query")
+    random_word = session.get("random_word", "-")
+    return render_template("hasil.html", random_word=random_word, query=query)
 
-if __name__ == '__main__':
+
+@app.route("/tambah-khodam", methods=["POST"])
+def tambah_khodam():
+    khodams = load_khodams()
+    khodam_baru = request.form.get("query_khodam")
+    if khodam_baru:
+        khodams.append(khodam_baru)
+        save_khodams(khodams)
+    return redirect(url_for("cek_khodam_hasil"))
+
+
+if __name__ == "__main__":
     app.run(debug=True)
